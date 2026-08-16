dd3a245d86ad5059bb12e1d7b4ec521addccc856
Julian Rodriguez <jrodriguezrpo@pm.me>
2026-08-15 19:56:51 -0500

    Add basic CDK TypeScript file

---
+++ deployment/typelevel-study-setup.ts
@@ -0,0 +1,63 @@
+import * as path from "node:path";
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as kinesis from "aws-cdk-lib/aws-kinesis";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+
+function requiredEnvironment(name: string): string {
+    const value = process.env[name];
+
+    if (!value) {
+        throw new Error(`${name} must be set`);
+    }
+
+    return value;
+}
+
+const lambdaMountDirectory = requiredEnvironment("LAMBDA_MOUNT_CWD");
+const lambdaHandler = requiredEnvironment("LAMBDA_HANDLER");
+
+if (!path.isAbsolute(llambdaMountDirectory)) {
+    throw new Error("LAMBDA_MOUNT_CWD must be an absolute path");
+}
+
+class TypeLevelLocalStack extends cdk.Stack {
+
+    constructor(scope: Construct, id: String, props: cdk.StackProps) {
+        super(scope, id, props);
+
+        const customersTable = new dynamodb.Table(this, "CustomersTable", {
+            tableName: "Customers",
+            partitionKey: {
+                name: "customerId",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        const couponsTable = new dynamodb.Table(this, "CouponsTable", {
+            tableName: "Coupons",
+            partitionKey: {
+                name: "couponCode",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+
+        const ordersTable = new dynamodb.Table(this, "OrdersTable", {
+            tableName: "Orders",
+            partitionKey: {
+                name: "orderId",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode,
+            stream: dynamodb.StreamViewType.NEW_IMAGE,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+    }
+}
